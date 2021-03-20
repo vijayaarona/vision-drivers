b5c0acffe7c52a45eaf81e4e7b917e3969607b43
Vijay Aaron A <vijay.boscosoft@outlook.com>
2021-03-20 16:27:53 +0530

    Vision Drivers Initial commit

---
+++ assets/js/ghstheme.script.js
@@ -0,0 +1,13 @@
+﻿// Intro carousel
+var introCarousel = $(".carousel");
+var introCarouselIndicators = $(".carousel-indicators");
+introCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
+    (index === 0) ?
+        introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") :
+        introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+});
+
+introCarousel.on('slid.bs.carousel', function (e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+});
